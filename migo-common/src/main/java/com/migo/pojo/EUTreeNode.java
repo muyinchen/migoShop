d8209955d837770cb228e8e1e8e4cf92314c8ae1
yinchen <fei6751803@163.com>
2016-09-26 23:57:54 +0800

    common模块

---
+++ migo-common/src/main/java/com/migo/pojo/EUTreeNode.java
@@ -0,0 +1,35 @@
+package com.migo.pojo;
+
+/**
+ * Author  知秋
+ * Created by kauw on 2016/9/23.
+ */
+public class EUTreeNode {
+    private long id;
+    private String text;
+    private String state;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ migo-common/src/main/java/com/migo/pojo/EasyUIDataGridResult.java
@@ -0,0 +1,28 @@
+package com.migo.pojo;
+
+import java.util.List;
+
+/**
+ * Author  知秋
+ * Created by kauw on 2016/9/23.
+ */
+public class EasyUIDataGridResult {
+    private long total;
+    private List<?> rows;
+
+    public List<?> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<?> rows) {
+        this.rows = rows;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+}
